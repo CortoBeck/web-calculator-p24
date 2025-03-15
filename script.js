6edc7bd89cd38c652eba662b756934ca43e91725
Corto Beck <corto.beck@etu.minesparis.psl.eu>
2025-03-15 18:38:47 +0100

    démarrage

--- script.js
+++ script.js
@@ -2,4 +2,21 @@
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
 // # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+// Corto Beck
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button'))
+        {
+
+        }
+    })
+
+const key = e.target
+const action = key.dataset.action
+
+if (!action) {
+    console.log('number key!')
+}
