0fe33b74f18c66a1bf5df5360f01438972e32bfd
Corto Beck <corto.beck@etu.minesparis.psl.eu>
2025-03-22 15:16:30 +0100

    keys

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ const calculate = (n1, operator, n2) => {
   
   return result;
 };
+
+keys.addEventListener('click', e => {
+  if (!e.target.matches('button')) return;
+  
+  const key = e.target;
+  const action = key.dataset.action;
+  const keyContent = key.textContent;
+  const displayedNum = display.textContent;
+  const previousKeyType = calculator.dataset.previousKeyType;
+
+  if (!action) {
+    if (displayedNum === '0' || previousKeyType === 'operator') {
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent;
+    }
+    calculator.dataset.previousKeyType = 'number';
+  }
+
+  if (action === 'decimal') {
+    if (!displayedNum.includes('.')) {
+      display.textContent = displayedNum + '.';
+    }
+    calculator.dataset.previousKeyType = 'decimal';
+  }
+
+});
+
