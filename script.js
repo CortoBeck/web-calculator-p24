0fe591a6928238c154e6e7587f0de0eacc874c68
Corto Beck <corto.beck@etu.minesparis.psl.eu>
2025-03-22 15:14:14 +0100

    opérateurs

--- script.js
+++ script.js
@@ -7,3 +7,19 @@
 const calculator = document.querySelector('.calculator');
 const display = calculator.querySelector('.calculator__display');
 const keys = calculator.querySelector('.calculator__keys');
+
+const calculate = (n1, operator, n2) => {
+  let result = '';
+  
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+  
+  return result;
+};
